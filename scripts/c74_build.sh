10058ebee241767610a5545c9df4ef2bb189a78c
Florian Demmer <florian@cycling74.com>
2021-05-10 20:32:30 +0100

    adjusted pre-build script

--- scripts/c74_build.sh
+++ scripts/c74_build.sh
@@ -11,10 +11,9 @@ function publish() {
 
 # test installing from source
 echo "building binaries for publishing"
-CFLAGS="${CFLAGS:-} -include $(pwd)/src/gcc-preinclude.h" CXXFLAGS="${CXXFLAGS:-} -include $(pwd)/src/gcc-preinclude.h" V=1 npm install --build-from-source  --clang=1
+CFLAGS="${CFLAGS:-} -include $(pwd)/src/gcc-preinclude.h" CXXFLAGS="${CXXFLAGS:-} -include $(pwd)/src/gcc-preinclude.h" V=1 npm install --build-from-source
 nm lib/binding/*/node_sqlite3.node | grep "GLIBCXX_" | c++filt  || true
 nm lib/binding/*/node_sqlite3.node | grep "GLIBC_" | c++filt || true
 npm test
-
 
 publish
